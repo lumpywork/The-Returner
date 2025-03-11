44391aa00afb329c8739a90f2c4aabfa3c528f00
lumpywork <juliancarter11804@gmail.com>
2025-03-11 14:17:05 -0500

    commit update the main class and changed the syntax in the input_handlers file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/.venv python3
 import tcod
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from input_handlers import EventHandler
 from entity import Entity
 
@@ -17,6 +17,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 -5), int(screen_height / 2), "@", (255,255,0))
     entities = {npc, player}
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -24,23 +25,11 @@ def main() -> None:
         title = "The Returner",
         vsync = True,
     )as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
-            context.present(root_console)
-            root_console.clear()
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy)
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
         main()
