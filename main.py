fe5aa3f8da7076f0f24cbc232d20a52caf92622d
lumpywork <juliancarter11804@gmail.com>
2025-03-11 16:07:41 -0500

    made dungeon generator

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tcod
 from engine import Engine
 from input_handlers import EventHandler
 from entity import Entity
-from game_map import GameMap
+from procgen import generate_dungeon
 
 def main() -> None:
     screen_width = 80
@@ -21,7 +21,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 -5), int(screen_height / 2), "@", (255,255,0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     engine = Engine(entities=entities, event_handler=event_handler,game_map=game_map, player=player)
     with tcod.context.new_terminal(
